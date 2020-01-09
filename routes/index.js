b74119baa79fd51d4b8ad35ef6d1a176b53261ab
Jeff Webb <jeffwebb@Jeff-Webbs-MBP.mshome.net>
2020-01-08 23:18:50 -0500

    First commit

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { name: 'Express' });
+});
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { name: 'About' });
+});
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { name: 'Contact' });
+});
+/* GET estimate page. */
+router.get('/estimate', function(req, res, next) {
+  res.render('estimate', { name: 'Estimate' });
+});
+/* GET photos page. */
+router.get('/photos', function(req, res, next) {
+  res.render('photos', { name: 'Photos' });
+});
+/* GET service area page. */
+router.get('/service-area', function(req, res, next) {
+  res.render('service-area', { name: 'Service Area' });
+});
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { name: 'Services' });
+});
+/* GET forms page. */
+router.get('/forms', function(req, res, next) {
+  res.render('forms', { name: 'Forms' });
+});
+
+module.exports = router;
