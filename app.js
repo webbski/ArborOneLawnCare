1fa7ccafe69a065c36233b20e73e8b5d1885009d
Jeff Webb <jeffwebb@Jeff-Webbs-MBP.mshome.net>
2020-01-13 12:15:36 -0500

    services routes added

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var servicesRouter = require('./routes/services');
 var app = express();
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
@@ -19,10 +20,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/services', servicesRouter);
 
 // catch 404 and forward to error handler
